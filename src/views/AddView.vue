380d72a7e4fcfa07277ba95abf732a9398f3c1f7
kwon <k2ohseung@gmail.com>
2022-04-20 10:43:39 +0900

    page

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-add page
+<h1>add page</h1>
+分類<input type="text"><br>
+種類<input type="text"><br>
+<p><button @click="addlist">追加</button></p>
   </div>
 </template>
 
 <script>
 
 export default {
-
+methods:{
+  addlist(){
+    return 
+  }
+}
 }
 </script>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    search page
+    search page<br>
+    分類<input type="text"><br>
+種類<input type="text"><br>
+<p><button @click="search">検索</button></p>
   </div>
 </template>
 
@@ -9,7 +12,11 @@
 
 
 export default {
-
+methods:{
+  search(){
+    return 
+  }
+}
   
 }
 </script>
