b72bf47bac0f357c0429cce56b945f3a283ba65d
kwon <k2ohseung@gmail.com>
2022-04-27 00:30:30 +0900

    map

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from "./store.js"
-
+// import * as VueGoogleMaps from "vue2-google-maps" // Import package
 
 Vue.config.productionTip = false
+
+// Vue.use(VueGoogleMaps, { 
+//   load: { key: "AIzaSyAsRf91TuMS99c-n80Zk8a3Q_MhZRCmqls", 
+//   libraries: "places", 
+//   region: "japan" }});
 
 new Vue({
   store,
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
-    <iframe
-  src="https://maps.google.co.jp/maps?output=embed&q=検索ワード"
+<!-- <Mapview/> -->
+    <!-- <iframe
+  src="https://maps.google.co.jp/maps?output=embed&q=コーナン"
   width="800"
   height="450"
   style="border: 0"
   allowfullscreen=""
   loading="lazy"
 >
-</iframe>
+</iframe> -->
   </div>
 </template>
 
 <script>
-
+// import Mapview from "@/components/AddGoogleMap.vue"
 export default {
+  components:{
+    // Mapview
+  },
+  data(){
   }
-
+}
 </script>
 
 <style>
