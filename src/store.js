0ba15b106cb724ff69be5e367d46dd79741321db
kwon <k2ohseung@gmail.com>
2022-05-23 11:23:48 +0900

    list

--- src/store.js
+++ src/store.js
@@ -7,10 +7,10 @@ const store = new Vuex.Store({
     infor:[
       {
         id:1,
-        category1:"その他",
-        category2:"その他",
-        name:"みそ",  
-        location0:"ライフ",
+        category1:"食材",
+        category2:"肉類",
+        name:"",  
+        location0:"",
         location1:"",
         infor_p0:"1",
         infor_p1:"300",
@@ -19,9 +19,9 @@ const store = new Vuex.Store({
       {
         id:2,
         category1:"日用品",
-        category2:"ティッシュ",
-        name:"ティッシュ",
-        location0:"コーナン",
+        category2:"厨房用品",
+        name:"",
+        location0:"",
         location1:"",
         infor_p0:20,
         infor_p1:1000,
@@ -30,9 +30,9 @@ const store = new Vuex.Store({
       {
         id:3,
         category1:"日用品",
-        category2:"トイレットペーパー",
-        name:"トイレットペーパー",
-        location0:"コーナン",
+        category2:"洗濯洗剤",
+        name:"",
+        location0:"",
         location1:"",
         infor_p0:50,
         infor_p1:500,
@@ -40,30 +40,30 @@ const store = new Vuex.Store({
       },
       {
         id:4,
-        category1:"食品",/* 大区分 */
-        category2:"加工品",/*  小区分*/
-        name:"ウインナー",/* 品名 */
-        location0:"ライフ 土佐堀店",/*販売先*/
+        category1:"食材",/* 大区分 */
+        category2:"魚類",/*  小区分*/
+        name:"",/* 品名 */
+        location0:"",/*販売先*/
         location1:{ lat: 34.68926290260607, lng: 135.49135386943817},/*販売先の座標*/
         infor_p0:22,/* 1個の値段 */
         infor_p1:1100,/* 販売値段 */
         infor_t1:50,/*  販売内容量*/
       },{
         id:5,
-        category1:"食品",
+        category1:"食材",
         category2:"調味料",
-        name:"醤油",
-        location0:"ライフ 西九条店",
+        name:"",
+        location0:"",
         location1:{ lat: 34.683140321434365, lng: 135.46425607526396},
         infor_p0:2.5,
         infor_p1:400,
         infor_t1:1000,
       },{
         id:6,
-        category1:"食品",
-        category2:"調味料",
-        name:"ケチャップ",
-        location0:"ライフ 西九条店",
+        category1:"医薬品",
+        category2:"酔い止め",
+        name:"",
+        location0:"",
         location1: { lat: 34.683140321434365, lng: 135.46425607526396},
         infor_p0:1,
         infor_p1:300,
@@ -71,10 +71,10 @@ const store = new Vuex.Store({
       },
       {
         id:7,
-        category1:"食品",
-        category2:"調味料",
-        name:"ケチャップ",
-        location0:"セブンイレブン 福島二丁目店",
+        category1:"医薬品",
+        category2:"痛み止め",
+        name:"",
+        location0:"",
         location1: { lat: 34.69499521550559 , lng: 135.4862194508314},
         infor_p0:400/300,
         infor_p1:400,
@@ -82,18 +82,40 @@ const store = new Vuex.Store({
       },
       {
         id:8,
-        category1:"食品",
-        category2:"調味料",
+        category1:"医薬品",
+        category2:"風邪薬",
         name:"痛み止め",
-        location0:"アカカベ　肥後橋店",
+        location0:"",
         location1: { lat: 34.69088923978887, lng: 135.49632235263374},
         infor_p0:1080/36,
         infor_p1:1080,
         infor_t1:36,
       },
+      {
+        id:9,
+        category1:"食材",
+        category2:"肉類",
+        name:"",  
+        location0:"",
+        location1:"",
+        infor_p0:"1",
+        infor_p1:"300",
+        infor_t1:"1",
+      },
+      {
+        id:10,
+        category1:"その他",
+        category2:"その他",
+        name:"",  
+        location0:"",
+        location1:"",
+        infor_p0:"1",
+        infor_p1:"300",
+        infor_t1:"1",
+      },
     
     ],
-    nextinforId:9,
+    nextinforId:11,
     filter:null
     },
   actions:{
