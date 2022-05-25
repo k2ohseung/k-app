fe6bab9474ebabfbdd514f058faca7fc97ebe528
kwon <k2ohseung@gmail.com>
2022-05-25 12:34:40 +0900

    test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from "./store.js"
+
 //google api key  AIzaSyAEdyDhwzgy5nc6QvL3fcvAbSyFBYivKaE
 import * as VueGoogleMaps from "vue2-google-maps" // Import package
+
+import './registerServiceWorker'
 
 
 
