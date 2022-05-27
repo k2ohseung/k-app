42494d5ffd68031bc34fc3ecfa2ea52ebbb9676f
kwon <k2ohseung@gmail.com>
2022-05-28 04:52:36 +0900

    game

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ol>
-      <li><a href="https://konishi-k.netlify.app/baseball">数字野球</a></li>
+      <li><a ref="https://konishi-k.netlify.app/baseball">数字野球</a></li>
     </ol>
     
   </div>
